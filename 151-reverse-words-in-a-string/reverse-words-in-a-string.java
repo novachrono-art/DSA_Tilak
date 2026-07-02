8df2e73f7279a763c03e734969a9a961d907e39b
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-07-02 23:24:27 +0530

    Time: 3 ms (98.88%) | Memory: 44.1 MB (87.02%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,17 +1,20 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder k = new StringBuilder();
-        int i = s.length()-1;
-        while(i>=0){
-            while(i>=0 && s.charAt(i)==' '){ i--; }
-            if(i<0) break;
-            int end =i;
-            while(i>=0 && s.charAt(i)!=' '){ i--; }
-            String word = s.substring(i+1,end+1);
-            if(k.length()>0) k.append(' ');
-            k.append(word);
-        }
-        
-        return k.toString();
+       StringBuilder k = new StringBuilder();
+    //    k to store the answer in the end 
+      int i=s.length()-1;
+      while(i>=0){
+        while(i>=0 && s.charAt(i)==' ') i--;
+        // ye string ke end me jitna bhi space hoGA USE Remove karega
+        if(i<0) break;
+        // iska use imp h if string gets empty it will program will still try to access words even though string is empty
+        int end=i;
+        while(i>=0 && s.charAt(i)!=' ')  i--;
+        // storing word in reverse order
+        String word = s.substring(i+1,end+1);
+        if(k.length()>0)  k.append(' ');
+        k.append(word);
+      }
+    return k.toString();
     }
 }
