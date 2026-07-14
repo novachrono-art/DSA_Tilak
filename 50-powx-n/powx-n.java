3fdddbd0be379c99b40cca714f14d1258e4ddf7e
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-07-14 18:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (34.82%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
-       if(n==0) return 1;
-        return Math.pow(x,n);
+       long N=n;
+       if(N<0){
+        x= 1/x;
+        N = -N;
+       }
+       return power(x,N);
+    }
+    public double power(double i,double j){
+        if(j==0) return 1.0;
+        if(j==1) return i;
+        if(j%2==0) return power(i*i,j/2);
+        return i*power(i,j-1);
+
     }
 }
