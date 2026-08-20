93d64b1e79f51fedc1f703384ce5ead48f88d55d
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-08-20 23:22:02 +0530

    Time: 2 ms (99.76%) | Memory: 53.6 MB (67.88%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,16 +1,13 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        Map<Integer,Integer> mp = new HashMap<>();
-        Set<Integer> s = new HashSet<>();
-        for(int i=0;i<trust.length;i++){
-            int v = trust[i][0];
-            int val = trust[i][1];
-            s.add(v);
-            mp.put(val,mp.getOrDefault(val,0)+1);
+        int[] sc = new int[n+1];
+        for(int[] i : trust){
+            sc[i[0]]--;
+            sc[i[1]]++;
         }
         for(int i=1;i<=n;i++){
-            if(mp.getOrDefault(i,0)==n-1 && !s.contains(i)){
-                 return i;
+            if(sc[i]==n-1){
+                return i;
             }
         }
         return -1;
