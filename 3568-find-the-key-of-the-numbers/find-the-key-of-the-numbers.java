818679e47812890a864f449feaa47d01d32858a8
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-08-24 23:27:07 +0530

    Time: 1 ms (98.30%) | Memory: 42.8 MB (45.63%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.java
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.java
@@ -1,18 +1,25 @@
 class Solution {
     public int generateKey(int num1, int num2, int num3) {
-       String n1 =String.format("%04d",num1);
-        String n2 =String.format("%04d",num2);
-         String n3 =String.format("%04d",num3);
+        int num=0;
+        int place=1;
+        while(num1!=0 &&num2!=0 && num3!=0){
+            int rem1=num1%10;
+            int rem2=num2%10;
+            int rem3=num3%10;
+            rem1=Math.min(rem1,rem2);
+            rem3=Math.min(rem1,rem3);
+            
+            num=num+(place*rem3);
+            place*=10;
+            num1/=10;
+            num2/=10;
+            num3/=10;
+
+        }
+
+       
+
+        return num;
         
-         int res=0;
-         for(int i=0;i<4;i++){
-            int p =n1.charAt(i)-'0';
-            int q =n2.charAt(i)-'0';
-            int r =n3.charAt(i)-'0';
-            int s = Math.min(p,Math.min(q,r));
-           
-            res=res*10+s;
-         }
-         return res;
     }
 }
