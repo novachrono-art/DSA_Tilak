6bbdce8eea460b56214780080374c5cf78bec059
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-08-27 14:11:51 +0530

    Time: 1 ms (76.60%) | Memory: 41.4 MB (99.79%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isThree(int n) {
+        int ct=0;
+        for(int i=1;i<=n;i++){
+          if(n%i==0) ct++;
+        }
+        return (ct==3)? true:false;
+    }
+}
