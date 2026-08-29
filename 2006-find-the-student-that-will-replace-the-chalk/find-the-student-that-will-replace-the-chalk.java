33c9e3e76d82f0016c91f3142074aac37687cb2a
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-08-29 11:20:13 +0530

    Time: 2 ms (93.48%) | Memory: 82.1 MB (36.34%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+       long sum=0;
+       for(int c: chalk){
+        sum+=c;
+       }
+       k=(int)(k%sum);
+       for(int i=0;i<chalk.length;i++){
+        if(k<chalk[i]) return i;
+        else k-=chalk[i];
+       }
+       return 0;
+    }
+}
