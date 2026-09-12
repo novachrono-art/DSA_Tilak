04afedc275f4027ba625a83b50716bbe0d34ae75
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-09-12 12:31:00 +0530

    Time: 5 ms (98.67%) | Memory: 44 MB (41.84%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -2,31 +2,17 @@ import java.util.HashMap;
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> mpp1 = new HashMap<>();
-        HashMap<Character, Character> mpp2 = new HashMap<>();
+       int[] fr1 = new int[256];
+       int[] fr2 = new int[256];
+       for(int i=0;i<s.length();i++){
+           char sc = s.charAt(i);
+           char tc  = t.charAt(i);
 
-        for (int i = 0; i < s.length(); i++) {
-            char sc = s.charAt(i);
-            char tc = t.charAt(i);
+           if(fr1[sc]!=fr2[tc]) return false;
 
-            if (mpp1.containsKey(sc)) {
-                if (mpp1.get(sc) != tc) {
-                    return false;
-                }
-            } else {
-                mpp1.put(sc, tc);
-            }
-
-            
-            if (mpp2.containsKey(tc)) {
-                if (mpp2.get(tc) != sc) {
-                    return false;
-                }
-            } else {
-                mpp2.put(tc, sc);
-            }
-        }
-
-        return true;
+           fr1[sc]=i+1;
+           fr2[tc]=i+1;
+       }
+       return true;
     }
 }
